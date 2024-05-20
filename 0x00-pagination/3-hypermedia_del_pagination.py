1149e0589b790b134b80a81354e22249fdf98136
hepona <hepona10@gmail.com>
2024-05-20 13:47:17 +0100

    3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,1 +1,55 @@
 #!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
+        """return a dictionary"""
+        assert isinstance(index, int)
+        assert isinstance(page_size, int) and page_size > 0
+        assert index >= 0 and index < len(self.__indexed_dataset)
+        data = []
+        tmp = index
+        for i in range(index, (page_size + index)):
+            if i in self.__indexed_dataset:
+                data.append(self.__indexed_dataset[i])
+                tmp = i
+        d = {
+            "index": index,
+            "next_index": tmp + 1,
+            "page_size": page_size,
+            "data": data,
+        }
+        return d
